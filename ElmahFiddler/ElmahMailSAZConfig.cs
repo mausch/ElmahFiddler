4ba3b348afcd72fe6e0eccad8962b0318ba69dfb
mausch <mauricioscheffer@gmail.com>
2010-03-26 04:23:20 +0000

    comments
    git-svn-id: http://mausch.googlecode.com/svn/trunk/ElmahFiddler@301 2e0ef560-8745-0410-9514-09e3b7f35b6e

--- ElmahFiddler/ElmahMailSAZConfig.cs
+++ ElmahFiddler/ElmahMailSAZConfig.cs
@@ -2,10 +2,29 @@
 using System.Text.RegularExpressions;
 
 namespace ElmahFiddler {
+    /// <summary>
+    /// Optional SAZ module configuration
+    /// </summary>
     public class ElmahMailSAZConfig {
+        /// <summary>
+        /// Password for SAZ file
+        /// </summary>
         public string Password { get; set; }
+
+        /// <summary>
+        /// List of URLs to exclude from SAZ
+        /// </summary>
         public IList<Regex> ExcludedUrls { get; private set; }
+
+        /// <summary>
+        /// Keep only the last N requests before the error
+        /// Only used by <see cref="ElmahMailSAZTraceModule"/>
+        /// </summary>
         public int? KeepLastNRequests { get; set; }
+
+        /// <summary>
+        /// Rename host in SAZ requests
+        /// </summary>
         public string RenameHost { get; set; }
 
         public ElmahMailSAZConfig() {
